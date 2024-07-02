b9b59395862302f7f5e1be14b0cbe690110c487e
zelin_mbp24 <zangzelin@gmail.com>
2024-07-02 20:32:42 +0800

    auto push

--- main.py
+++ main.py
@@ -3,35 +3,56 @@ import torchvision
 from matplotlib import pyplot as plt
 from tsimcne.tsimcne import TSimCNE
 
-# get the cifar dataset (make sure to adapt `data_root` to point to your folder)
-data_root = "/zangzelin/data/cifar-10"
-dataset_train = torchvision.datasets.CIFAR10(
-    root=data_root,
-    download=True,
-    train=True,
-)
-dataset_test = torchvision.datasets.CIFAR10(
-    root=data_root,
-    download=True,
-    train=False,
-)
-dataset_full = torch.utils.data.ConcatDataset([dataset_train, dataset_test])
+import wandb
+import argparse
 
-# create the object (here we run t-SimCNE with fewer epochs
-# than in the paper; there we used [1000, 50, 450]).
-# tsimcne = TSimCNE(total_epochs=[500, 50, 250])
-tsimcne = TSimCNE(total_epochs=[1000, 50, 450])
+def main():
+    
+    # argparse
+    parser = argparse.ArgumentParser(description='t-SimCNE')
+    parser.add_argument('--epochs', type=int, nargs='+', default=[1000, 50, 450], help='epochs for each stage')
+    parser.add_argument('--seed', type=int, default=0, help='random seed')
+    parser.add_argument('--devices', type=int, default=1, help='number of devices')
+    
+    args = parser.parse_args()
+    
+    
+    
+    wandb.init(project="tsimcne")
 
-# train on the augmented/contrastive dataloader (this takes the most time)
-tsimcne.fit(dataset_full)
+    # get the cifar dataset (make sure to adapt `data_root` to point to your folder)
+    data_root = "/zangzelin/data/cifar-10"
+    dataset_train = torchvision.datasets.CIFAR10(
+        root=data_root,
+        download=True,
+        train=True,
+    )
+    dataset_test = torchvision.datasets.CIFAR10(
+        root=data_root,
+        download=True,
+        train=False,
+    )
+    dataset_full = torch.utils.data.ConcatDataset([dataset_train, dataset_test])
 
-# map the original images to 2D
-Y = tsimcne.transform(dataset_full)
+    # create the object (here we run t-SimCNE with fewer epochs
+    # than in the paper; there we used [1000, 50, 450]).
+    # tsimcne = TSimCNE(total_epochs=[500, 50, 250])
+    tsimcne = TSimCNE(
+        total_epochs=args.epochs,
+        devices=args.devices,
+        )
 
-# get the original labels from the dataset
-labels = [lbl for img, lbl in dataset_full]
+    # train on the augmented/contrastive dataloader (this takes the most time)
+    tsimcne.fit(dataset_full)
 
-# plot the data
-fig, ax = plt.subplots()
-ax.scatter(*Y.T, c=labels)
-fig.savefig("tsimcne[1000, 50, 450].png")
+    # map the original images to 2D
+    Y = tsimcne.transform(dataset_full)
+
+    # get the original labels from the dataset
+    labels = [lbl for img, lbl in dataset_full]
+
+    # plot the data
+    fig, ax = plt.subplots()
+    ax.scatter(*Y.T, c=labels)
+    wandb.log({"tsne": plt})
+    wandb.finish()
