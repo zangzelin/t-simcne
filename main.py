d2feaaf0e1aebd59d83e2c4f2f59fcbdecdb45fc
zelin_mbp24 <zangzelin@gmail.com>
2024-07-02 17:04:14 +0800

    auto push

---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision
+from matplotlib import pyplot as plt
+from tsimcne.tsimcne import TSimCNE
+
+# get the cifar dataset (make sure to adapt `data_root` to point to your folder)
+data_root = "experiments/cifar/out/cifar10"
+dataset_train = torchvision.datasets.CIFAR10(
+    root=data_root,
+    download=True,
+    train=True,
+)
+dataset_test = torchvision.datasets.CIFAR10(
+    root=data_root,
+    download=True,
+    train=False,
+)
+dataset_full = torch.utils.data.ConcatDataset([dataset_train, dataset_test])
+
+# create the object (here we run t-SimCNE with fewer epochs
+# than in the paper; there we used [1000, 50, 450]).
+tsimcne = TSimCNE(total_epochs=[500, 50, 250])
+
+# train on the augmented/contrastive dataloader (this takes the most time)
+tsimcne.fit(dataset_full)
+
+# map the original images to 2D
+Y = tsimcne.transform(dataset_full)
+
+# get the original labels from the dataset
+labels = [lbl for img, lbl in dataset_full]
+
+# plot the data
+fig, ax = plt.subplots()
+ax.scatter(*Y.T, c=labels)
+fig.savefig("tsimcne.png")
