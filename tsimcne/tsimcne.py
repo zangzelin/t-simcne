8d588a62e28eacaf1721256264277241a6b1f3cb
zelin_mbp24 <zangzelin@gmail.com>
2024-07-02 20:44:26 +0800

    auto push

--- tsimcne/tsimcne.py
+++ tsimcne/tsimcne.py
@@ -532,6 +532,7 @@ class TSimCNE:
                 max_epochs=n_epochs,
                 devices=self.devices,
                 **self.trainer_kwargs,
+                accelerator="ddp" if self.devices > 1 else None,
             )
             trainer.fit(model=plmodel, train_dataloaders=train_dl)
             self.models.append(plmodel)
