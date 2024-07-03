e429702d42e93a51d8dfba103bf04c66ed721486
zelin_mbp24 <zangzelin@gmail.com>
2024-07-03 10:39:08 +0800

    auto push

--- tsimcne/models/simclr_like.py
+++ tsimcne/models/simclr_like.py
@@ -334,6 +334,64 @@ class ResNet(nn.Module):
         return out
 
 
+class MLPZZL(nn.Module):
+    def __init__(
+        self, block, num_blocks, in_channel=3, zero_init_residual=False
+    ):
+        super(MLPZZL, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(
+            in_channel, 64, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode="fan_out", nonlinearity="relu"
+                )
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch, so that
+        # the residual branch starts with zeros, and each residual
+        # block behaves like an identity. This improves the model by
+        # 0.2~0.3% according to: https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for i in range(num_blocks):
+            stride = strides[i]
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x, layer=100):
+        import pdb; pdb.set_trace()
+        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        return out
+
+
 class AdaptedConvNet(nn.Module):
     """Creates a torchvision model with a modified stride.
 
@@ -370,6 +428,8 @@ def resnet18(**kwargs):
 def resnet34(**kwargs):
     return ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
 
+def mlpresnet(**kwargs):
+    return MLPZZL(BasicBlock, [3, 4, 6, 3], **kwargs)
 
 def resnet50(**kwargs):
     return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
@@ -402,6 +462,7 @@ def efficientnet_v2_l(**kwargs):
 model_dict = {
     "resnet18": [pytorch_resnet, 512],
     "resnet18_simclr": [resnet18, 512],
+    "mlp_simclr": [mlpresnet, 512],
     "resnet34": [resnet34, 512],
     "resnet50": [resnet50, 2048],
     "resnet101": [resnet101, 2048],
